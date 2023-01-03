af955878b191423072df342862ee550f5a7caa05
Abraham Olamipo <aolamipo@teamapt.com>
2023-01-03 03:34:27 +0100

    LB: Testing spring cloud client loadbalancer

--- src/main/java/com/pluralsight/fastpassui/FastpassUiApplication.java
+++ src/main/java/com/pluralsight/fastpassui/FastpassUiApplication.java
@@ -3,9 +3,11 @@ package com.pluralsight.fastpassui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@LoadBalancerClient(name = "fastpass-service", configuration = LoadBalancerConfig.class)
 @SpringBootApplication
 public class FastpassUiApplication {
 
